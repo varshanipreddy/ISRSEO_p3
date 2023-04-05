33f684cd9b99970a83c45b730fb3fe1342b8e980
varshani reddy <varshanireddy@varshanis-MacBook-Pro.local>
2023-04-05 15:15:01 -0500

    commit frontend

---
+++ my-isr-seo/src/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="container">
+      <h1>Welcome to my page!</h1>
+      <p>This is my page about wpaskjbdn zqnakjsnde.</p>
+    </div>
+  );
+}
+
+export default HomePage;
